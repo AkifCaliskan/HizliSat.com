a3262471b825d66dd744e30c43605feb19a70b1c
Akif Çalışkan <101815696+AkifCaliskan@users.noreply.github.com>
2025-01-03 00:37:29 +0300

    ..

---
+++ SahibindenUi/Pages/Login/SignUp/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SahibindenUi.Pages.Login.SignUp
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
